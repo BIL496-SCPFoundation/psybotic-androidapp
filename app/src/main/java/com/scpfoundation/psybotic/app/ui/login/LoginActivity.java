db3e7871ce2b8a63ba24845a76a8f9c6ea0d0366
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2021-03-02 00:21:09 +0300

    Main page initial design

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/login/LoginActivity.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/login/LoginActivity.java
@@ -2,27 +2,15 @@ package com.scpfoundation.psybotic.app.ui.login;
 
 import android.app.Activity;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -31,16 +19,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.scpfoundation.psybotic.app.R;
-import com.scpfoundation.psybotic.app.ui.login.LoginViewModel;
-import com.scpfoundation.psybotic.app.ui.login.LoginViewModelFactory;
 import com.scpfoundation.psybotic.app.ui.main.MainActivity;
+import com.scpfoundation.psybotic.app.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int RC_SIGN_IN = 100;
-    //    private static final int RC_SIGN_IN = ;
-    //    private LoginViewModel loginViewModel;
     private GoogleSignInClient mGoogleSignInClient;
 
     private void signIn() {
@@ -51,6 +35,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onStart() {
         super.onStart();
+        /*
+         * Check if user already signed in via a google account, if yes go to MainActivity
+         */
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if (account != null) {
             updateUI(account);
@@ -93,6 +80,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        System.out.println("Oluştu");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Configure sign-in to request the user's ID, email address, and basic
@@ -105,93 +94,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.google_login_button).setOnClickListener(this);
         findViewById(R.id.guest_login_button).setOnClickListener(this);
 
-//        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-//                .get(LoginViewModel.class);
-//
-//        final EditText usernameEditText = findViewById(R.id.login_email);
-//        final EditText passwordEditText = findViewById(R.id.login_password);
-//        final Button loginButton = findViewById(R.id.login_button);
-//        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-//        loadingProgressBar.setVisibility(View.INVISIBLE);
-//
-//        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-//            @Override
-//            public void onChanged(@Nullable LoginFormState loginFormState) {
-//                if (loginFormState == null) {
-//                    return;
-//                }
-//                loginButton.setEnabled(loginFormState.isDataValid());
-//                if (loginFormState.getUsernameError() != null) {
-//                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-//                }
-//                if (loginFormState.getPasswordError() != null) {
-//                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-//                }
-//            }
-//        });
-//
-//        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-//            @Override
-//            public void onChanged(@Nullable LoginResult loginResult) {
-//                if (loginResult == null) {
-//                    return;
-//                }
-//                loadingProgressBar.setVisibility(View.GONE);
-//                if (loginResult.getError() != null) {
-//                    showLoginFailed(loginResult.getError());
-//                }
-//                if (loginResult.getSuccess() != null) {
-//                    updateUiWithUser(loginResult.getSuccess());
-//                }
-//                setResult(Activity.RESULT_OK);
-//
-//                //Complete and destroy login activity once successful
-//                finish();
-//            }
-//        });
-//
-//        TextWatcher afterTextChangedListener = new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//                // ignore
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                // ignore
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-//                        passwordEditText.getText().toString());
-//            }
-//        };
-//        usernameEditText.addTextChangedListener(afterTextChangedListener);
-//        passwordEditText.addTextChangedListener(afterTextChangedListener);
-//        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if (actionId == EditorInfo.IME_ACTION_DONE) {
-//                    loginViewModel.login(usernameEditText.getText().toString(),
-//                            passwordEditText.getText().toString());
-//                }
-//                return false;
-//            }
-//        });
-//
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                loadingProgressBar.setVisibility(View.VISIBLE);
-//                loginViewModel.login(usernameEditText.getText().toString(),
-//                        passwordEditText.getText().toString());
-//                Intent intent = new Intent(getActivity(v.getContext()), MainActivity.class);
-//                startActivity(intent);
-//
-//            }
-//        });
     }
 
     public Activity getActivity(Context context)
@@ -230,13 +132,4 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-//    private void updateUiWithUser(LoggedInUserView model) {
-//        String welcome = getString(R.string.welcome) + model.getDisplayName();
-//        // TODO : initiate successful logged in experience
-//        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-//    }
-//
-//    private void showLoginFailed(@StringRes Integer errorString) {
-//        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-//    }
 }
