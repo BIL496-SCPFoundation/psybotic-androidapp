4ca78d867ec9d45159c8dcdd31d50b9155c063cd
Grisworld <oguzandas@gmail.com>
2021-03-02 02:38:29 +0300

    ChatBot and PsychologyActivity are added.

---
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/chatbot/ChatBotActivity.java
@@ -0,0 +1,16 @@
+package com.scpfoundation.psybotic.app.ui.chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.scpfoundation.psybotic.app.R;
+
+public class ChatBotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+    }
+}
