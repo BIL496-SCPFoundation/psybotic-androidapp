bf7d4fcf7f24eaccf3e2a6e13741a263151629ae
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2021-03-03 00:29:27 +0300

    Google login integration to main server

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/login/LoginActivity.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.scpfoundation.psybotic.app.ui.login;
 import android.app.Activity;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -13,6 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,13 +29,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+import com.scpfoundation.psybotic.app.ui.data.User;
+import com.scpfoundation.psybotic.app.ui.handler.LoginHandler;
 import com.scpfoundation.psybotic.app.ui.main.MainActivity;
 import com.scpfoundation.psybotic.app.R;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,
+        Response.Listener<JSONObject>, Response.ErrorListener{
 
     private static final int RC_SIGN_IN = 100;
     private GoogleSignInClient mGoogleSignInClient;
+    RequestQueue requestQueue;
+    private final String HOST = "http://192.168.0.12:8080";
+    private ProgressDialog dialog;
+    private GoogleSignInAccount account;
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -62,10 +79,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            updateUI(account);
+            final User user = new User();
+            user.setEmail(account.getEmail());
+            user.setFirstName(account.getGivenName());
+            user.setLastName(account.getFamilyName());
+            user.setGoogleId(account.getId());
+            JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, HOST + "/users/login",
+                    null, this, this) {
+                @Override
+                public byte[] getBody() {
+                    Gson gson = new Gson();
+                    String body = gson.toJson(user);
+                    return body.getBytes();
+                }
+            };
+            dialog = ProgressDialog.show(this, "",
+                    "Loading. Please wait...", true);
+            requestQueue.add(req);
+            requestQueue.start();
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -87,9 +121,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onCreate(Bundle savedInstanceState) {
         System.out.println("Oluştu");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        requestQueue = Volley.newRequestQueue(this.getApplicationContext());
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -117,4 +151,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        dialog.cancel();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        dialog.cancel();
+        updateUI(account);
+    }
 }
