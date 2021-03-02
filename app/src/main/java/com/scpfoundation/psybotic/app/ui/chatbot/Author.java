f5aab00bb8c3c6754c64109073523734c55b4f81
Grisworld <oguzandas@gmail.com>
2021-03-03 01:36:58 +0300

    Some modics are added.

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/chatbot/Author.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/chatbot/Author.java
@@ -1,29 +1,4 @@
 package com.scpfoundation.psybotic.app.ui.chatbot;
 
-public class Author implements IUser {
-
-    String id;
-    String name;
-    String avatar;
-
-    public Author(){
-        id = null;
-        name = null;
-        avatar = null;
-    }
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String getAvatar() {
-        return avatar;
-    }
+public class Author {
 }
