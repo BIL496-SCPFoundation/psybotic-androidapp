b5328d42c2df2c95fe5bf4b818d41d1ce2b91902
Grisworld <oguzandas@gmail.com>
2021-03-28 00:20:14 +0300

    Psychologist Screen has been bounded PsychologyActivity and MainActivity and a bind completed between these and some modifications are added.

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/main/MainActivity.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/main/MainActivity.java
@@ -19,6 +19,7 @@ import com.scpfoundation.psybotic.app.R;
 import com.scpfoundation.psybotic.app.ui.login.LoginActivity;
 import com.scpfoundation.psybotic.app.ui.profile.ProfileActivity;
 import com.scpfoundation.psybotic.app.ui.chatbot.ChatBotActivity;
+import com.scpfoundation.psybotic.app.ui.psychologychat.PsychologistsNames;
 import com.scpfoundation.psybotic.app.ui.psychologychat.PsychologyActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<Void>{
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.psychologist_chat_view:
                 // go to psychologist chat page
-                Intent intentforPsychology = new Intent(this.getApplicationContext(), PsychologyActivity.class);
+                Intent intentforPsychology = new Intent(this.getApplicationContext(), PsychologistsNames.class);
                 startActivity(intentforPsychology);
                 break;
             case R.id.notifications_view:
