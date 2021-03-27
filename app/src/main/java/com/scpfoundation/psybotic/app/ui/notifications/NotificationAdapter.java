ba6d7a46030da75da858e4ff0f139dd0d4524731
beratyavas19 <i.beratyavas@gmail.com>
2021-03-27 13:51:08 +0300

    connected psychat screen

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/notifications/NotificationAdapter.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/notifications/NotificationAdapter.java
@@ -2,6 +2,7 @@ package com.scpfoundation.psybotic.app.ui.notifications;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +11,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.scpfoundation.psybotic.app.R;
 import com.scpfoundation.psybotic.app.data.Notification;
+import com.scpfoundation.psybotic.app.ui.profile.ProfileActivity;
+import com.scpfoundation.psybotic.app.ui.psychologychat.PsychologyActivity;
 
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder>  {
     private static final String HOST = "https://limitless-lake-96203.herokuapp.com";
     private ProgressDialog dialog;
 
@@ -60,6 +64,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             public void onClick(View v) {
                // RequestQueue requestQueue = Volley.newRequestQueue(iyiyim.getContext());;
                 String url = HOST + "/notifications/update";
+                deleteItem(position,holder);
                 /*JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url,
                         null, new Response.Listener<JSONObject>() {
                     @Override
@@ -100,6 +105,9 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             public void onClick(View v) {
                 //RequestQueue requestQueue = Volley.newRequestQueue(iyiyim.getContext());;
                 String url = HOST + "/notifications/update";
+                Intent intent = new Intent(v.getContext(), PsychologyActivity.class);
+                deleteItem(position,holder);
+                v.getContext().startActivity(intent);
                 /*
                 JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url,
                         null, new Response.Listener<JSONObject>() {
@@ -171,6 +179,12 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 
     public NotificationAdapter(List<Notification> notificationList) {
         this.notificationList = notificationList;
+    }
+    private void deleteItem(int position,ViewHolder holder) {
+        notificationList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, notificationList.size());
+        holder.itemView.setVisibility(View.GONE);
     }
 
 
