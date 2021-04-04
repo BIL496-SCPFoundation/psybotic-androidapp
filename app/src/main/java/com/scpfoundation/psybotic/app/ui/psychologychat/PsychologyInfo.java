7be9b6182e2cd99af8e4867d25ab930b410a7376
Grisworld <oguzandas@gmail.com>
2021-04-04 22:11:11 +0300

    A new screen has been added to make bind with PsychologyActivity and PsychologistsNames(will be developed sooner!)

---
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/psychologychat/PsychologyInfo.java
@@ -0,0 +1,16 @@
+package com.scpfoundation.psybotic.app.ui.psychologychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.scpfoundation.psybotic.app.R;
+
+public class PsychologyInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_psychology_info);
+    }
+}
