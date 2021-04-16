cac78cac6374e5fe867d60abbbb2b9555b8ebec0
Grisworld <oguzandas@gmail.com>
2021-04-16 23:23:43 +0300

    PsychologyTable works better but still modifying...

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/psychologychat/PsychologistsNames.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/psychologychat/PsychologistsNames.java
@@ -6,8 +6,11 @@ import androidx.cardview.widget.CardView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -32,9 +35,8 @@ public class PsychologistsNames extends AppCompatActivity {
         int cardview_count = 0;
         try {
             JSONArray arr = new JSONArray(body);
-            LinearLayout templinearlayout;
+            LinearLayout templinearlayout = null;
             for(int i = 0; i < arr.length(); i++){
-                //System.out.println(arr.length() + " BANGA  "+arr.getJSONObject(i));
                 if(cardview_count % 2 == 0) {
                     LinearLayout newlayout = new LinearLayout(this.getApplicationContext());
                     newlayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -42,15 +44,10 @@ public class PsychologistsNames extends AppCompatActivity {
                             LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                     newlayout.layout(0,20,0,0);
                     CardView cardView = new CardView(this.getApplicationContext());
-                    CardView.MarginLayoutParams p = (CardView.MarginLayoutParams) cardView.getLayoutParams();
-                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)
-                            cardView.getLayoutParams();
-                    p.setMarginStart(20);
-                    params.weight = 0.3f;
-                    cardView.setLayoutParams(p);
-                    cardView.setLayoutParams(params);
-                    cardView.getLayoutParams().width=0;
-                    cardView.getLayoutParams().height=150;
+
+
+                    cardView.setLayoutParams(new LinearLayout.LayoutParams(0, 150,
+                            0.3f));
                     cardView.setClickable(true);
                     cardView.setFocusable(true);
                     cardView.setOnClickListener(new View.OnClickListener() {
@@ -66,10 +63,76 @@ public class PsychologistsNames extends AppCompatActivity {
                     });
                     cardView.setRadius(50);
 
+                    LinearLayout innLayout = new LinearLayout(c);
+                    innLayout.setPadding(0,0,0,20);
+                    innLayout.setOrientation(LinearLayout.VERTICAL);
+                    innLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
 
+                    ImageView imageView = new ImageView(c);
+                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT,0,0.8f));
+
+                    TextView textView = new TextView(c);
+                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT,0,0.2f));
+                    textView.setGravity(Gravity.CENTER);
+                    textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    textView.setTextSize(15);
+                    textView.setText(("Dr."+arr.getJSONObject(i).getString("firstName")+" "+
+                            arr.getJSONObject(i).get("lastName")));
+
+                    innLayout.addView(imageView);
+                    innLayout.addView(textView);
+                    cardView.addView(innLayout);
+                    newlayout.addView(cardView);
+                    templinearlayout = newlayout;
 
                 }
                 else{
+                    CardView cardView = new CardView(this.getApplicationContext());
+
+                    cardView.setLayoutParams(new LinearLayout.LayoutParams(0, 150,
+                            0.3f));
+                    cardView.setClickable(true);
+                    cardView.setFocusable(true);
+                    cardView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(c, PsychologyInfo.class);
+                            CardView cardView = (CardView) v;
+                            TextView textView = (TextView) ((LinearLayout) cardView.
+                                    getChildAt(0)).getChildAt(1);
+                            intent.putExtra("psychology_name", textView.getText().toString());
+                            startActivity(intent);
+                        }
+                    });
+                    cardView.setRadius(50);
+
+                    LinearLayout innLayout = new LinearLayout(c);
+                    innLayout.setPadding(0,0,0,20);
+                    innLayout.setOrientation(LinearLayout.VERTICAL);
+                    innLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+
+                    ImageView imageView = new ImageView(c);
+                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT,0,0.8f));
+
+                    TextView textView = new TextView(c);
+                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.
+                            LayoutParams.MATCH_PARENT,0,0.2f));
+                    textView.setGravity(Gravity.CENTER);
+                    textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    textView.setTextSize(15);
+                    textView.setText(("Dr."+arr.getJSONObject(i).getString("firstName")+" "+
+                            arr.getJSONObject(i).get("lastName")));
+
+                    innLayout.addView(imageView);
+                    innLayout.addView(textView);
+                    cardView.addView(innLayout);
+                    templinearlayout.addView(cardView);
+                    ln.addView(templinearlayout);
 
                 }
 
