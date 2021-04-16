81307951ef78d587441b09751e9ac1e542d9212d
Grisworld <oguzandas@gmail.com>
2021-04-17 02:03:49 +0300

    Cloning is tried to make better code for PsychologyTable but failed...

--- app/src/main/java/com/scpfoundation/psybotic/app/ui/psychologychat/PsychologistsNames.java
+++ app/src/main/java/com/scpfoundation/psybotic/app/ui/psychologychat/PsychologistsNames.java
@@ -1,17 +1,22 @@
 package com.scpfoundation.psybotic.app.ui.psychologychat;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.Layout;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -19,21 +24,47 @@ import com.scpfoundation.psybotic.app.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import java.lang.Cloneable;
 import java.util.Arrays;
 
+public class PsychologistsNames extends AppCompatActivity implements Cloneable{
 
-public class PsychologistsNames extends AppCompatActivity {
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_psychologists_names);
         String body = (String) getIntent().getExtras().get("psychologists");
-        LinearLayout ln = findViewById(R.id.all_screen);
+        LinearLayout linear_all = findViewById(R.id.all_screen);
         Context c = this.getApplicationContext();
-        int cardview_count = 0;
+        LinearLayout main_layout = (LinearLayout) linear_all.getChildAt(2);
+        CardView child = (CardView) main_layout.getChildAt(0);
+        CardView child2 = (CardView) main_layout.getChildAt(1);
+        linear_all.removeView(linear_all.getChildAt(2));
+        main_layout.removeView(child2);
+        main_layout.removeView(child);
+        LinearLayout tempLayout = null;
         try {
+            JSONArray arr = new JSONArray(body);
+            for(int i = 0; i < arr.length(); i++){
+
+                LinearLayout linearLayout = (LinearLayout) child.getChildAt(0);
+                TextView textView = (TextView) linearLayout.getChildAt(1);
+                textView.setText("Dr. "+arr.getJSONObject(i).get("firstName")+ " "+arr.
+                        getJSONObject(i).get("lastName"));
+
+                linear_all.addView(main_layout);
+                main_layout.addView(child);
+
+
+            }
+
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+
+
+        /*try {
             JSONArray arr = new JSONArray(body);
             LinearLayout templinearlayout = null;
             for(int i = 0; i < arr.length(); i++){
@@ -137,13 +168,10 @@ public class PsychologistsNames extends AppCompatActivity {
                 }
 
                 cardview_count++;
-            }
+            }*/
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
     }
-
 
     public void onClick(View v) {
         Intent intent = new Intent(this.getApplicationContext(), PsychologyInfo.class);
@@ -152,4 +180,6 @@ public class PsychologistsNames extends AppCompatActivity {
         intent.putExtra("psychology_name", textView.getText().toString());
         startActivity(intent);
     }
+
+
 }
